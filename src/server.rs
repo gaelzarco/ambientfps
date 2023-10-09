1df3acdbf1d46ea19f9c21dda118dec16bd4b106
gael <76548719+gaelzarco@users.noreply.github.com>
2023-10-08 19:58:40 -0700

    cubes and spheres

--- src/server.rs
+++ src/server.rs
@@ -1,16 +1,34 @@
 use ambient_api::{
     core::{
         rendering::components::color,
-        primitives::components::quad,
-        transform::components::scale,
-        physics::components::plane_collider, model::components::model_from_url, player::components::is_player,
+        primitives::{
+            components::{quad, cube},
+            concepts::Sphere
+        },
+        transform::{
+            components::{scale, translation},
+            concepts::Transformable
+        },
+        physics::components::{
+            plane_collider,
+            cube_collider,
+            sphere_collider,
+            dynamic
+        },
+        model::components::model_from_url,
+        player::components::is_player, ecs::components::remove_at_game_time,
     },
     prelude::*,
 };
-use packages::{character_controller::components::use_character_controller, character_animation::components::basic_character_animations};
+
+use packages::{
+    character_controller::components::use_character_controller,
+    character_animation::components::basic_character_animations, this::components::bouncy_created
+};
 
 #[main]
 pub fn main() {
+    // Plane
     Entity::new()
         .with(quad(), ())
         .with(scale(), Vec3::ONE * 10.0)
@@ -18,6 +36,7 @@ pub fn main() {
         .with(plane_collider(), ())
         .spawn();
 
+    // Spawn Query
     spawn_query(is_player()).bind(move |players| {
         for (id, _) in players {
             entity::add_components(
@@ -29,4 +48,34 @@ pub fn main() {
             );
         }
     });
+
+    for _ in 0..30 {
+        Entity::new()
+            .with(cube(), ())
+            .with(cube_collider(), Vec3::ONE)
+            .with(translation(), (random::<Vec2>()* 10.0 - 10.0).extend(1.))
+            .spawn();
+    }
+
+    fixed_rate_tick(Duration::from_secs_f32(0.5), |_| {
+        Entity::new()
+            .with_merge(Sphere::suggested())
+            .with_merge(Transformable::suggested())
+            .with(scale(), Vec3::ONE * 0.2)
+            .with(translation(), Vec3::X * 10. + (random::<Vec2>() * 2.0 - 1.0).extend(10.))
+            .with(sphere_collider(), 0.5)
+            .with(dynamic(), true)
+            .with(bouncy_created(), game_time())
+            .with(remove_at_game_time(), game_time() + Duration::from_secs_f32(5.0))
+            .spawn();
+    });
+
+    // THIS IS HOW remove_at_game_time() works under the hood
+    // query(bouncy_created()).each_frame(|entities| {
+    //     for (id, created) in entities {
+    //         if (game_time() - created).as_secs_f32() > 5.0 {
+    //             entity::despawn(id);
+    //         }
+    //     }
+    // });
 }
